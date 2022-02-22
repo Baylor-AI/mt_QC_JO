b9944a8095204593eab21c67858bf11dfa173475
Bikram Khanal <bikram.khanal@aiesec.net>
2022-02-22 15:15:24 -0600

    Updated Hadamard

--- Quantum_tutorial.py
+++ Quantum_tutorial.py
@@ -1,1 +1,9 @@
-import pennylane
+import numpy as np
+
+ket_0 = np.array([1,0])
+ket_1 = np.array([0,1])
+print("The dot product between two states: ",np.dot(ket_0,ket_1))
+hadamard = 1/np.sqrt(2) * np.array([[1,1],[1,-1]])
+print(hadamard * hadamard)
+print(np.dot(hadamard,ket_0))
+print(np.dot(hadamard,ket_1))
